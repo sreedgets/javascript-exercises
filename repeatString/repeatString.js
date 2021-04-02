f3ad0d0bd7bbcbde269f8cf2e401d7ece81b200a
Skyler <sreedgets@outlook.com>
2021-04-02 08:10:42 -0500

    Work on excercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function( string, num ) {
+    if ( num < 0 ) {
+        return 'ERROR';
+    } else {
+        let strToRepeat = string;
+        let newStr = '';
+        for (let i = num; i > 0; i--) {
+            newStr = newStr + strToRepeat;
+        }
+        return newStr;
+    }
 }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function( str ) {
+    let strToArr = str.split("");
+    let newStr = strToArr.reverse().join("");
 
+    return newStr;
 }
 
 module.exports = reverseString
