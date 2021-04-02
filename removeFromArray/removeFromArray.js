7a76a637756418786888c3255f636e57e1d6494c
Skyler <sreedgets@outlook.com>
2021-04-02 11:59:03 -0500

    Work on removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function( arr, el ) {
+    const index = arr.indexOf( el );
+    arr.splice( index, 1 );
+    return arr;
 }
 
 module.exports = removeFromArray
